4076048d6120e9568e87c0b3a2dd82074f30a531
raj_l <rajan.lee97@gmail.com>
2023-07-30 19:07:20 -0400

    Added error handling when given ticker symbol that doesn't exist

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 @app.route('/ticker/', methods=['GET'])
 def getData():
+
 	ticker = str(request.args.get('ticker'))  # so query string would be something like /ticker/?ticker=MSFT
 	headers = {
 		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36'}
@@ -16,11 +17,13 @@ def getData():
 	r = requests.get(url, headers=headers)
 
 	soup = BeautifulSoup(r.text, 'html.parser')
-
-	current = soup.select_one(f'fin-streamer[data-symbol="{ticker}"][data-field="regularMarketPrice"]')
-	# current['value']
-	data = {'Price': current['value']}
-	json_dump = json.dumps(data)
+	try:
+		current = soup.select_one(f'fin-streamer[data-symbol="{ticker}"][data-field="regularMarketPrice"]')
+		data = {'Price': current['value']}
+		json_dump = json.dumps(data)
+	except:
+		data = {'Price': 'Price not found. Please recheck ticker symbol.'}
+		json_dump = json.dumps(data)
 	return json_dump
 
 
